895849a024d76daac84cbbb6ecd969c053fd331b
Alex Wajda <alexander.wajda@gmail.com>
2017-03-07 14:40:37 +0100

    Clean up the code and add examples

--- project/plugins.sbt
+++
@@ -1,4 +0,0 @@
-logLevel := Level.Warn
-
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.3")
-addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
--- src/main/scala/com/mpc/scalats/CLI.scala
+++ src/main/scala/com/mpc/scalats/CLI.scala
@@ -4,34 +4,21 @@ import java.io.{File, PrintStream}
 
 import com.mpc.scalats.configuration.Config
 import com.mpc.scalats.core.TypeScriptGenerator
+import CLIOpts._
 
-object Main {
-
-  sealed abstract class OptKey(val key: String)
-
-  case object SrcClassNames extends OptKey(null)
-
-  case object OutFile extends OptKey("--out")
-
-  case object EmitInterfaces extends OptKey("--emit-interfaces")
-
-  case object EmitClasses extends OptKey("--emit-classes")
-
-  case object OptionToNullable extends OptKey("--option-to-nullable")
-
-  case object OptionToUndefined extends OptKey("--option-to-undefined")
+object CLI {
 
   def main(args: Array[String]) {
     val options = parseArgs(args.toList)
 
     val classNames: List[String] = options get SrcClassNames match {
-      case Some(cns: List[String]) => cns
+      case Some(cns) => cns
       case _ => printUsage()
     }
 
     val out: PrintStream = options get OutFile match {
       case None => System.out
-      case Some(outFile: File) =>
+      case Some(outFile) =>
         outFile.getAbsoluteFile.getParentFile.mkdirs()
         outFile.delete()
         new PrintStream(outFile)
@@ -48,16 +35,16 @@ object Main {
     finally out.close()
   }
 
-  private def parseArgs(args: List[String]): Map[OptKey, Any] = {
+  private def parseArgs(args: List[String]): CLIOpts = {
     args match {
-      case Nil => Map.empty
+      case Nil => CLIOpts.empty
       case OutFile.key :: outFileName :: restArgs => parseArgs(restArgs) + (OutFile -> new File(outFileName))
       case EmitInterfaces.key :: restArgs => parseArgs(restArgs) + (EmitInterfaces -> true)
       case EmitClasses.key :: restArgs => parseArgs(restArgs) + (EmitClasses -> true)
       case OptionToNullable.key :: restArgs => parseArgs(restArgs) + (OptionToNullable -> true)
       case OptionToUndefined.key :: restArgs => parseArgs(restArgs) + (OptionToUndefined -> true)
       case key :: _ if key startsWith "-" => printUsage(s"Unknown option $key")
-      case classNames => Map(SrcClassNames -> classNames)
+      case classNames => CLIOpts(SrcClassNames -> classNames)
     }
   }
 
